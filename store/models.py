ff309946028cd5e52f74c4958401efe7e4825317
hacker777-cris <crispusgikonyo@gmail.com>
2024-07-11 00:24:58 +0300

    Add descriptions field

--- store/models.py
+++ store/models.py
@@ -5,9 +5,12 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField('Product', through='CartItem', related_name='carts')
+    products = models.ManyToManyField(
+        "Product", through="CartItem", related_name="carts"
+    )
 
     def get_total_price(self):
         total_price = 0
@@ -15,23 +18,26 @@ class Cart(models.Model):
             total_price += item.product.price * item.quantity
         return total_price
 
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
-    # Add other fields as needed
+    image = models.ImageField(upload_to="product_images/", null=True, blank=True)
+    description = models.TextField(
+        default="This is product description"
+    )  # Add this field
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, default='hello')
+    name = models.CharField(max_length=100, default="hello")
 
     class Meta:
         verbose_name_plural = "categories"
@@ -43,33 +49,41 @@ class Category(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
-    email = models.EmailField(default='example@example.com')
-    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
+    email = models.EmailField(default="example@example.com")
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures", blank=True, null=True
+    )
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
 
+
 class Order(models.Model):
     PAYMENT_CHOICES = (
-        ('online', 'Online Payment'),
-        ('delivery', 'Pay on Delivery'),
+        ("online", "Online Payment"),
+        ("delivery", "Pay on Delivery"),
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     products = models.ManyToManyField(Product)
-    payment_method = models.CharField(max_length=20, choices=PAYMENT_CHOICES, default='online')
+    payment_method = models.CharField(
+        max_length=20, choices=PAYMENT_CHOICES, default="online"
+    )
     delivery_location = models.CharField(max_length=50)
 
     def __str__(self):
@@ -82,4 +96,6 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return f"Order #{self.order.id} - {self.product.name} - Quantity: {self.quantity}"
+        return (
+            f"Order #{self.order.id} - {self.product.name} - Quantity: {self.quantity}"
+        )
